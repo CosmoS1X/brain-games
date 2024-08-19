c0aa4565fcb0335be25a33f0ec1fcc1faf93a881
Konstantin Korovin <cosmo.stan@yandex.com>
2024-08-19 17:46:53 +0300

    feat: add a greeting by name

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export default () => {
+import readlineSync from 'readline-sync';
+
+export default (): void => {
   console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
 };
