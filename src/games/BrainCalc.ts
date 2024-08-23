74ccfeb64a75677033b4a3703936b95b71c06137
Konstantin Korovin <cosmo.stan@yandex.com>
2024-08-23 20:23:53 +0300

    refactor: refactor "BrainCalc.ts"
    Move "calculate" and "getRandomOperator" functions into the class as private methods

--- src/games/BrainCalc.ts
+++ src/games/BrainCalc.ts
@@ -3,35 +3,35 @@ import getRandomInteger from '../getRandomInteger';
 
 type OperatorsUnion = '+' | '-' | '*';
 
-const calculate = (leftOperand: number, rightOperand: number, operator: OperatorsUnion): number => {
-  switch (operator) {
-    case '+':
-      return leftOperand + rightOperand;
-    case '-':
-      return leftOperand - rightOperand;
-    case '*':
-      return leftOperand * rightOperand;
-    default:
-      throw new Error('Received unknown operator');
-  }
-};
-
-const getRandomOperator = (): OperatorsUnion => {
-  const operators: OperatorsUnion[] = ['+', '-', '*'];
-  const randomIndex = getRandomInteger(0, operators.length - 1);
-  return operators[randomIndex];
-};
-
 export default class BrainCalc extends GameEngine {
   constructor() {
     super('What is the result of the expression?');
   }
 
+  private calculate(leftOperand: number, rightOperand: number, operator: OperatorsUnion): number {
+    switch (operator) {
+      case '+':
+        return leftOperand + rightOperand;
+      case '-':
+        return leftOperand - rightOperand;
+      case '*':
+        return leftOperand * rightOperand;
+      default:
+        throw new Error('Received unknown operator');
+    }
+  }
+
+  private getRandomOperator(): OperatorsUnion {
+    const operators: OperatorsUnion[] = ['+', '-', '*'];
+    const randomIndex = getRandomInteger(0, operators.length - 1);
+    return operators[randomIndex];
+  }
+
   generateQuestionAndAnswer() {
     const leftOperand = getRandomInteger(1, 99);
     const rightOperand = getRandomInteger(1, 99);
-    const operator = getRandomOperator();
+    const operator = this.getRandomOperator();
     this.setQuestion(`${leftOperand} ${operator} ${rightOperand}`);
-    this.setAnswer(String(calculate(leftOperand, rightOperand, operator)));
+    this.setAnswer(String(this.calculate(leftOperand, rightOperand, operator)));
   }
 }
